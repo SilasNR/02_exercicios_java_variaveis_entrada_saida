1bf834723ded60f424d6f1fd4c5bb9aef2d7c75c
Silas Rodrigues Nascimento <silasnas2018@gmail.com>
2023-09-08 16:23:56 -0300

    ap02(Exercícios): 03 finalizado

---
+++ Comissao.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Comissao{
+    public static void main (String [] args){
+        //Declaração
+        double comissao;
+        double quantidade;
+        double preco;
+
+        //Entrada
+        preco = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o preço do produto :"));
+
+        quantidade = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite a quantidade :"));
+
+        //Processamento
+
+        comissao = (preco * quantidade) * 0.05;
+
+        //Saida
+        JOptionPane.showMessageDialog(null,comissao);
+
+
+
+    }
+}
